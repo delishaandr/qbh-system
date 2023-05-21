757326634079650886c38f4f1bae1f9c561c1af2
delishaandr <13519133@std.stei.itb.ac.id>
2023-05-21 21:30:23 +0700

    transpose hum, get min cost from transpositions

--- src/subsequence_dtw.py
+++ src/subsequence_dtw.py
@@ -3,6 +3,23 @@ import libfmp.c3
 import libfmp.c7
 import numpy as np
 
+def songRanking(hum, midi):
+    iter = []
+
+    for i in range (12):
+        costs = []
+        for j in range (len(midi)):
+            costs.append(subsequenceDTW(hum, midi[j]))
+        iter.append(costs)
+        hum = transposeHum(hum)
+    
+    tr_iter = np.array(iter).T.tolist()
+    min_iter = getMinIter(tr_iter)
+    min_iter.sort(key=lambda x: x[1])
+    rank_iter = np.array(min_iter).T.tolist()
+
+    return rank_iter[0]
+
 def subsequenceDTW(arr1, arr2):
     cost_matrix = libfmp.c3.compute_cost_matrix(arr1, arr2, metric='euclidean')
     acc_cost_matrix = libfmp.c7.compute_accumulated_cost_matrix_subsequence_dtw_21(cost_matrix)
@@ -20,6 +37,24 @@ def subsequenceDTW(arr1, arr2):
     # print('Accumulated cost D[N, b*] =', acc_cost_matrix[-1, b_ast])
 
     return acc_cost_matrix[-1, b_ast]
+
+def getMinIter(iter):
+    min = []
+    for i in range(len(iter)):
+        s = iter[i]
+        s.sort()
+        min.append([i, s[0]])
+
+    return min
+
+def transposeHum(hum):
+    tr = []
+    for h in hum:
+        if (h == 11):
+            tr.append(0)
+        else:
+            tr.append(h+1)
+    return tr
 
 def meanReciprocalRank(ranks, labels):
     Q = len(labels)
