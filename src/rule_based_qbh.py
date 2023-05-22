3bcec3f0a29c2f94ee545d12174336ddb873e951
delishaandr <13519133@std.stei.itb.ac.id>
2023-05-22 12:00:32 +0700

    rule based qbh (baseline)

---
+++ src/rule_based_qbh.py
@@ -0,0 +1,19 @@
+import math
+
+def ruleBasedQBH(all_freqs):
+    labels = []
+    for freqs in all_freqs:
+        labels.append(getFreqLabels(freqs))
+    
+    return labels
+
+def getFreqLabels(freqs):
+    label = []
+    for freq in freqs:
+        if (freq == 0):
+            label.append(0)
+        else:
+            m = round(12 * math.log(freq / 440, 2) + 69)
+            label.append((m % 12 + 3) % 12)
+    
+    return label
